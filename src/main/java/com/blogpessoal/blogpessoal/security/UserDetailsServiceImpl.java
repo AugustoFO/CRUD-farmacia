252ffa28d50cc71002555993e81ce7d0e4753d75
Felix Augusto <111309593+AugustoFO@users.noreply.github.com>
2025-01-27 10:16:25 -0300

    Enviando arquivos necessários para deploy

---
+++ src/main/java/com/blogpessoal/blogpessoal/security/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package com.blogpessoal.blogpessoal.security;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.crudfarmacia.crud_farmacia.model.Usuario;
+import com.crudfarmacia.crud_farmacia.repository.UsuarioRepository;
+
+
+
+@Component
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+		Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+		if (usuario.isPresent())
+			return new UserDetailsImpl(usuario.get());
+		else
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+			
+	}
+}
